b4ebfa48ff9dd573f889447042c7a7ae85840ec8
Marijn <93777269+Unkn0wn-Fear@users.noreply.github.com>
2023-01-16 11:29:54 +0100

    fixed white screen
    wit scherm gefixt spheres worden nu goed uitgeprint op het scherm

--- C++Eindopdracht/classes.cpp
+++ C++Eindopdracht/classes.cpp
@@ -11,225 +11,230 @@ namespace st = std;
 namespace eindopdracht{
 
 
-Vec3D::Vec3D(float x, float y, float z):
-    x(x),y(y),z(z)
-{
+    Vec3D::Vec3D(float x, float y, float z):
+            x(x),y(y),z(z)
+    {
 
-}
+    }
 
-void Vec3D::show (st::string label) const{
-    //variablen van dit type afdrukt in een console window, gelabeld met hun naam en afgesloten
-    //met een newline
-          std::cout << label << ' ' << x << ' ' << y << ' ' << z;
-              std::cout << '\n';
-}
-
- 
-void Vec3D::show (st::string label, float scalar) const{
-    //die floating point
-    //scalars afdrukt in een console window, gelabeld met hun naam en afgesloten met een
-    //newline+
-
-         std::cout << label << ' ' << scalar << ' ';
-             std::cout << '\n';
-}
-
- 
-void Vec3D::show() const{
-      std::cout << '\n';
-
-}
-
- 
-Vec3D Vec3D::minus () const {
-    //die een vector returnt die de
-    //andere kant op wijst als self
-return Vec3D(-x, -y, -z);
-}
+    void Vec3D::show (st::string label) const{
+        //variablen van dit type afdrukt in een console window, gelabeld met hun naam en afgesloten
+        //met een newline
+        std::cout << label << ' ' << x << ' ' << y << ' ' << z;
+        std::cout << '\n';
+    }
 
 
+    void Vec3D::show (st::string label, float scalar) const{
+        //die floating point
+        //scalars afdrukt in een console window, gelabeld met hun naam en afgesloten met een
+        //newline+
 
- 
+        std::cout << label << ' ' << scalar << ' ';
+        std::cout << '\n';
+    }
 
-Vec3D Vec3D::add ( Vec3D const &other) const{
 
-    // de som van self en other returnt
-  return Vec3D(x + other.x,  y + other.y,  z + other.z);
- 
+    void Vec3D::show() const{
+        std::cout << '\n';
 
-}
+    }
 
- 
 
-Vec3D Vec3D::sub ( Vec3D const &other) const{
+    Vec3D Vec3D::minus () const {
+        //die een vector returnt die de
+        //andere kant op wijst als self
+        return Vec3D(-x, -y, -z);
+    }
 
- //het verschil van self en other returnt
 
-  return Vec3D( x - other.x,  y - other.y,  z - other.z);
 
-}
 
- 
 
-Vec3D Vec3D::mul (float scalar) const{
+    Vec3D Vec3D::add ( Vec3D const &other) const{
+
+        // de som van self en other returnt
+        return Vec3D(x + other.x,  y + other.y,  z + other.z);
+
+
+    }
+
+
+
+    Vec3D Vec3D::sub ( Vec3D const &other) const{
+
+        //het verschil van self en other returnt
+
+        return Vec3D( x - other.x,  y - other.y,  z - other.z);
+
+    }
+
+
+
+    Vec3D Vec3D::mul (float scalar) const{
 
 //het product van self en scalar returnt
 
-return Vec3D( x * scalar,  y * scalar,  z * scalar);
+        return Vec3D( x * scalar,  y * scalar,  z * scalar);
 
-}
+    }
 
- 
 
-Vec3D Vec3D::div (float scalar) const{
+
+    Vec3D Vec3D::div (float scalar) const{
 
 //het quotient van self en scalar returnt
 
-   return Vec3D( x / scalar,  y / scalar,  z / scalar);
- 
-
-}
+        return Vec3D( x / scalar,  y / scalar,  z / scalar);
 
 
- 
-
-float Vec3D::norm () const{
-
-    //de norm (lengte) van self returnt
-
-return sqrt(pow( x, 2) + pow( y, 2) + pow( z, 2));
- 
-
-}
-
- Vec3D Vec3D::unit () const{
-
-    //en vector returnt met dezelfde richting als z'n self, maar met lengte 1
-    auto normsvector = norm();
-    return Vec3D( x / normsvector,  y / normsvector,  z / normsvector);
- }
-
- float Vec3D::dot ( Vec3D const &other) const{
-
-    // het inproduct (dot product) van self en other returnt
-
-    return ( x * other.x) + ( y * other.y) + ( z * other.z);
- }
-
- Vec3D Vec3D::cross (Vec3D const &other) const{
-
-    //het uitproduct (cross product) van self en other returnt
-      return Vec3D(( y*other.z -  z*other.y), ( z*other.x -  x*other.z), ( x*other.y -  y*other.x));
-  }
-
-
-Sphere::Sphere (float x, float y, float z, float radius) : Object(x,y,z) {
-    center.x = x;
-    center.y = y;
-    center.z = z;
-    radius = radius;
-    
- }
-Floor::Floor(float x, float y, float z) : Object(x,y,z)  {
-    center.x = x;
-    center.y = y;
-    center.z = z;
-}
-
-Ray::Ray (float xSup, float ySup, float zSup, float xDir, float yDir, float zDir){
-     support.x = xSup;
-     support.y = ySup;
-     support.z = zSup;
-     direction.x = xDir;
-     direction.y = yDir;
-     direction.z = zDir;
-}
-
-
-float Sphere::distFromRay (Ray const &ray)const
-{
-    return ray.support.sub (center).cross(ray.direction).norm ();
-}
+    }
 
 
 
-Vec3D Sphere::hitPoint(Ray const &ray){
+
+    float Vec3D::norm () const{
+
+        //de norm (lengte) van self returnt
+
+        return sqrt(pow( x, 2) + pow( y, 2) + pow( z, 2));
+
+
+    }
+
+    Vec3D Vec3D::unit () const{
+
+        //en vector returnt met dezelfde richting als z'n self, maar met lengte 1
+        auto normsvector = norm();
+        return Vec3D( x / normsvector,  y / normsvector,  z / normsvector);
+    }
+
+    float Vec3D::dot ( Vec3D const &other) const{
+
+        // het inproduct (dot product) van self en other returnt
+
+        return ( x * other.x) + ( y * other.y) + ( z * other.z);
+    }
+
+    Vec3D Vec3D::cross (Vec3D const &other) const{
+
+        //het uitproduct (cross product) van self en other returnt
+        return Vec3D(( y*other.z -  z*other.y), ( z*other.x -  x*other.z), ( x*other.y -  y*other.x));
+    }
+
+
+    Sphere::Sphere(float x, float y, float z, float radius) : Object(x, y, z), radius(radius)
+    {
+        center = Vec3D(x,y,z);
+
+    }
+    Floor::Floor(float x, float y, float z) : Object(x,y,z)  {
+        center.x = x;
+        center.y = y;
+        center.z = z;
+    }
+
+    Ray::Ray (float xSup, float ySup, float zSup, float xDir, float yDir, float zDir){
+        support.x = xSup;
+        support.y = ySup;
+        support.z = zSup;
+        direction.x = xDir;
+        direction.y = yDir;
+        direction.z = zDir;
+    }
+
+    Ray::Ray(float xStart, float yStart, VPO &objects) : objects(objects) {
+        support=Vec3D(0,0,3);
+        direction = Vec3D(xStart, yStart, 0).sub(support);
+
+    }
+
+
+    float Sphere::distFromRay (Ray const &ray)const
+    {
+        return ray.support.sub (center).cross(ray.direction).norm ();
+    }
+
+
+
+    Vec3D Sphere::hitPoint(Ray const &ray){
         auto blabla = ray.support.sub(center);
         auto nable = pow(ray.direction.dot(blabla), 2) - pow(blabla.norm(), 2) + pow(radius, 2);
         auto distFromSupport = -ray.direction.dot(blabla) - sqrt(nable);
         return ray.support.add(ray.direction.mul(distFromSupport));
     }
 
-Object::Object (float x, float y, float z): center (x, y, z) {}
+    Object::Object (float x, float y, float z): center (x, y, z) {}
 
     //Deze method test in derived classes of het betreffende object geraakt wordt door de straal en
     //wat de support vector en de direction vector van de weerkaatste straal zijn.
 
     bool Ray::scan() {
         for (auto OBJ : objects) {
+
             if (OBJ->hit(*this)) {
                 return true;
             }
         }
         return false;
-}
+
+    }
 
 
-bool Sphere::hit(Ray const &ray) const{
-return distFromRay(ray) <= radius;
-}
+    bool Sphere::hit(Ray const &ray) const{
+        return distFromRay(ray) <= radius;
+    }
 
-bool Object::hit(Ray const &ray)const {
-}
+    bool Object::hit(Ray const &ray)const {}
 
-bool Floor::hit(Ray const &ray) const
-{
-}
-
-
-   void RayScanner::scan() {
-auto const RijXas = 96; 
-auto const RijYas = 3 * RijXas;
-auto const aspectRatio = 0.65;
-auto const ScreenDist = 3.00;
-
-auto const ascii = " M";
+    bool Floor::hit(Ray const &ray) const
+    {
+    }
 
 
+    void RayScanner::scan() {
+        auto const RijXas = 96;
+        auto const RijYas = 3 * RijXas;
+        auto const aspectRatio = 0.65;
+        auto const ScreenDist = 3.00;
 
+        auto const ascii = std::string(" M");
 
+        auto const minRadius = 0.2;
+        auto const maxRadius = 0.4;
 
-auto const minRadius = 0.2;
-auto const maxRadius = 0.4;
+        auto image = VVF (RijXas, VF(RijYas));
 
-auto image = VVF (RijXas, VF(RijYas));
+        for (auto rowIndex = 0; rowIndex < RijXas;rowIndex++) {
+            auto y = (RijXas / 2. - rowIndex) / RijXas;
+            for (auto colIndex = 0; colIndex < RijYas; colIndex++) {
+                auto x = (RijYas / 2 - colIndex ) / (aspectRatio * RijYas);
+                auto VisionRay = Ray(x,y,objects);
+            
 
- for (auto rowIndex = 0; rowIndex < RijXas;rowIndex++) {
-    auto y = (RijXas / 2. - rowIndex) / RijXas;
-        for (auto colIndex = 0; colIndex < RijYas; colIndex++) {  
-           auto x = (RijYas / 2 - colIndex ) / (aspectRatio * RijYas);
-            auto direction = Vec3D(x, y, 0).sub(Vec3D(0, 0, -ScreenDist));
-            Ray VisionRay(0, 0, ScreenDist, direction.x, direction.y, direction.z);
-                VisionRay.scan();
                 if (VisionRay.scan()== true ) {
+                    //std::cout << "a";
                     image [rowIndex][colIndex] = 1;
                 }
-                else {
+                else if(VisionRay.scan()== false) {
+                    //std::cout << "b";
                     image [rowIndex][colIndex] = 0;
                 }
 
+            }
         }
-  }
-    for (auto rowIndex = 0; rowIndex < RijXas; rowIndex++) {
-        for (auto colIndex = 0; colIndex < RijYas; colIndex++) {
-            if(image [rowIndex][colIndex] == 1)
-                std::cout << ascii[0];
-            else
-                std::cout << ascii[1];
+        for (auto rowIndex = 0; rowIndex < RijXas; rowIndex++) {
+            for (auto colIndex = 0; colIndex < RijYas; colIndex++) {
+                if (image [rowIndex][colIndex] == 1) {
+                    std::cout << ascii[1];
+                }
+                else {
+                    std::cout << ascii[0];
+                }
+            }
+            std::cout << '\n';
         }
         std::cout << '\n';
     }
-    std::cout << '\n';
 
-    }
 }
---
+++ C++Eindopdracht/cmake-build-debug/CMakeFiles/C__Eindopdracht.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Eindopdracht.dir/Main.cpp.obj"
+  "CMakeFiles/C__Eindopdracht.dir/classes.cpp.obj"
+  "C__Eindopdracht.exe"
+  "C__Eindopdracht.exe.manifest"
+  "C__Eindopdracht.pdb"
+  "libC__Eindopdracht.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Eindopdracht.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
